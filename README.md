32c3fe36c9d8111cc2e1dbefac8185baf46d6090
ksh44820@likelion.org <ksh44820@likelion.org>
2020-01-14 18:53:56 +0900

    서버 코드 작성

--- README.md
+++ README.md
@@ -10,8 +10,8 @@ UI를 서버에서 렌더링하는 것.
 >서버 리소스 사용 - 서버 과부하 발생 가능
 >SSR을 하면 프로젝트의 구조가 더 복잡해질 수 있고, 데이터 미리 불러오기, 코드 스플리팅과의 호환 등 고려해야 할 사항이 많아짐.
 
->Loadable Component 라이브러리에서 제공하는 기능을 사용해 SSR 후 
->필요한 파일의 경로를 추추하여 렌더링 결과에 스크립트/스타일 태그를 삽입
+Loadable Component 라이브러리에서 제공하는 기능을 사용해 SSR 후 
+필요한 파일의 경로를 추추하여 렌더링 결과에 스크립트/스타일 태그를 삽입
 
 ## 웹팩 설정
 > 1. eject
@@ -23,6 +23,17 @@ UI를 서버에서 렌더링하는 것.
 > 7. 환경 변수 주입
 
 ## 빌드 스크립트 작성
-package.json scripts 부분 확인
-$ yarn build:server
-$ yarn start:server
+>package.json scripts 부분 확인
+>$ yarn build:server
+>$ yarn start:server
+
+## 서버 코드 작성하기
+>SSR을 처리할 서버를 작성, Express 프레임워크 사용
+>
+>#### StaticRouter
+>주로 SSR 용도로 사용되는 라우터
+>props로 넣어주는 location 값에 따라 라우팅해 줌.
+>context라는 props는 렌더링 컴포넌트에 따라 HTTP 상태 코드 설정.
+
+>빌드, 서버 실행 후 network 탭에서 SSR이 제대로 이루어졌는지 확인 가능.
+
--- src/index.server.js
+++ src/index.server.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 
-const html = ReactDOMServer.renderToString(
-    <div>Hello Server Side Rendering!!</div>
-)
+import express from 'express';
+import { StaticRouter } from 'react-router-dom';
+import App from './App';
 
-console.log(html);
+const app = express();
+
+// SSR을 처리할 핸들러 함수.
+const serverRender = (req, res, next) =>{
+    // 이 함수는 404가 떠야하는 상황에서 404를 띄우지 않고
+    // SSR을 해줌.
+    const context = {};
+    const jsx = (
+        // StaticRouter 주로 SSR 용도로 사용되는 라우터
+        <StaticRouter location={req.url} context={context}>
+            <App/>
+        </StaticRouter>
+    );
+    const root = ReactDOMServer.renderToString(jsx); // 렌더링
+    res.send(root); // 클라이언트에 결과물 응답
+}
+
+app.use(serverRender);
+
+// 4000번 포트로 서버 가동
+app.listen(4000, ()=>{
+    console.log('Running on https://localhost:4000');
+})
